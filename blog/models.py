fc76988ef765e6d444664afb973342e136ffc95d
mikola-s <suharev.nv@gmail.com>
2019-12-29 20:54:40 +0200

    blog/models add Author

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Author(AbstractUser):
+    birth_date = models.DateField(null=True, blank=True)
+    create_date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.get_username()
