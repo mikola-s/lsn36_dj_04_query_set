84c5532ead4ffbcd6ee0937cc52dc05f72c6c3e7
mikola-s <suharev.nv@gmail.com>
2019-12-29 19:42:52 +0200

    blog/views.py add index

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('blog/index.html')
+    return render(request, template)
