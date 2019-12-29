fd07675346ee808eb2ec65f20e4e84eb58b027c3
mikola-s <suharev.nv@gmail.com>
2019-12-29 23:44:43 +0200

    blog/admin.py add Article Comments Expression ExpressionType

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,7 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Author)
+admin.site.register(models.Article)
+admin.site.register(models.Comments)
+admin.site.register(models.Expression)
+admin.site.register(models.ExpressionType)
